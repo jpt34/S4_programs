1e54125dd291d19b0fbb8fe1d18b39453070b986
jpt34 <107045909+jpt34@users.noreply.github.com>
2024-07-11 13:04:56 +0100

    Add files via upload

---
+++ LDpredRuns/ClusterSchizophreniaDosages.R
@@ -0,0 +1,28 @@
+library(bigsnpr)
+library(tidyverse)
+library(bigreadr)
+
+ukb <- readRDS("RefFiles/biobank_ldpred_all.rds.rds")
+G<-ukb$genotypes
+CHR<-as.integer(ukb$map$chromosome)
+sumstats = readRDS("RefFiles/schizophrenia.rds")
+NCORES=16
+
+for (chr in 1:22) {
+  
+  ind.chr <- which(sumstats$chr == chr)
+  
+  if (chr == 1) {
+    df_beta <- sumstats[ind.chr, c("beta", "beta_se", "n_eff", "_NUM_ID_")]
+  } else {
+    df_beta <- rbind(df_beta, sumstats[ind.chr, c("beta", "beta_se", "n_eff", "_NUM_ID_")])
+  }
+}
+
+beta_grid_tibble=read_delim("beta_schizophrenia_grid_values.txt"," ",col_names=FALSE)
+beta_grid=as.matrix(beta_grid_tibble)
+
+bigparallelr::set_blas_ncores(NCORES)
+pred_grid <- big_prodMat(G, beta_grid, ind.col = df_beta[["_NUM_ID_"]])
+
+write.table(pred_grid, "pred_schizophrenia_grid_values.txt", row.names=FALSE, col.names=FALSE, sep="\t")
